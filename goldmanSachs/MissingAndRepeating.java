7b372f33413f251dc5d88a26804859f8bb99b9e3
aj235711 <amirjaved68504@gmail.com>
2022-01-05 17:11:55 +0530

    missing and repeating 2

--- goldmanSachs/MissingAndRepeating.java
+++ goldmanSachs/MissingAndRepeating.java
@@ -1,28 +1,23 @@
 package goldmanSachs;
 
 import java.util.*;
-import java.math.BigInteger;
 
 class Solution {
     int[] findTwoElement(int arr[], int n) {
         // code here
-        BigInteger N = BigInteger.valueOf(n);
-        BigInteger one = BigInteger.valueOf(1);
-        BigInteger two = BigInteger.valueOf(2);
-        BigInteger six = BigInteger.valueOf(6);
-        BigInteger sum = (N.multiply(N.add(one))).divide(two);
-        BigInteger sqSum = ((N.multiply(N.add(one))).multiply((two.multiply(N)).add(one))).divide(six);
-        System.out.println(sum + " " + sqSum);
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum.subtract(BigInteger.valueOf(arr[i]));
-            sqSum = sqSum.subtract(BigInteger.valueOf(arr[i]).multiply(BigInteger.valueOf(arr[i])));
-        }
-        int s = sum.intValue();
-        int ss = sqSum.intValue();
         int ans[] = new int[2];
-        int div = ss / s;
-        ans[1] = (s + div) / 2;
-        ans[0] = (div - s) / 2;
+        int map[] = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            map[arr[i]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (map[i] == 0) {
+                ans[1] = i;
+            }
+            if (map[i] > 1) {
+                ans[0] = i;
+            }
+        }
         return ans;
     }
 }
